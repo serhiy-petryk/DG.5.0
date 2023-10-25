d8b10b7abb69d7f5fe0c58c5d35e72318af85748
serhiy-petryk <sergei_petrik@yahoo.com>
2023-10-26 01:28:27 +0300

    Update DataProjectTests.cs

--- DG.5.0/TestsConsole/DataTests/DataProjectTests.cs
+++ DG.5.0/TestsConsole/DataTests/DataProjectTests.cs
@@ -6,7 +6,7 @@
         {
             var connString = "SqlClient;Data Source=localhost;Initial Catalog=dbOneSAP_DW;Integrated Security=True;Connection Timeout=300;Encrypt=false";
 
-            using (var conn = Data.DB.DbHelper.Connection_Get(connString))
+            /*using (var conn = Data.DB.DbHelper.Connection_Get(connString))
             // using (var cmd = Data.DB.Helper.Command_Get(conn, "SELECT * from dbQ2023.dbo.SymbolsPolygon where symbol=@symbol", null))
             // using (var cmd = Data.DB.DbHelper.Command_Get(conn, "select b.altacc altacc1, * from dbOneSAP_DW..mast_coa a left join dbOneSAP_DW..mast_coa_alt b on a.altacc = b.altacc;", null))
             using (var cmd = Data.DB.DbHelper.Command_Get(conn, "CUBE_GLDOCLINE_WITH_BALANCE", null))
@@ -25,7 +25,7 @@
 
                     }
                 }
-            }
+            }*/
 
         }
     }
