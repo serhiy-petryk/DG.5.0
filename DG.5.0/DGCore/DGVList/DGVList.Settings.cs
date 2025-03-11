f5b044d06cfad4f0d0dfa494c9ec6cee78f9029a
serhiy-petryk <sergei_petrik@yahoo.com>
2025-03-11 21:06:55 +0200

    reformatted some files

--- DG.5.0/DGCore/DGVList/DGVList.Settings.cs
+++ DG.5.0/DGCore/DGVList/DGVList.Settings.cs
@@ -5,80 +5,80 @@ using DGCore.Misc;
 
 namespace DGCore.DGVList
 {
-  public partial class DGVList<TItem>
-  {
-    // 3. Add Settings for DGVList (split ApplySettings for DGVList/DGVCube); remove public sets for some DGVList properties
-    public void ResetSettings()
+    public partial class DGVList<TItem>
     {
-      var whereFilter = (UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)WhereFilter;
-      whereFilter?.ApplySetting(whereFilter.GetBlankSetting());
-      var filterByValue = (UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)FilterByValue;
-      filterByValue?.ApplySetting(filterByValue.GetBlankSetting());
-      ShowTotalRow = false;
-      ExpandedGroupLevel = 0;
-      ShowGroupsOfUpperLevels = false;
-      TextFastFilter = null;
-
-      Groups?.Clear();
-      Sorts?.Clear();
-      SortsOfGroups?.Clear();
-      TotalLines = new TotalLine[0];
-    }
-
-    public void SetSettings(UserSettings.DGV settingInfo)
-    {
-      TextFastFilter = settingInfo.TextFastFilter;
-
-      WhereFilter.ClearFilter();
-      if (settingInfo.WhereFilter != null && settingInfo.WhereFilter.Count > 0)
-        ((UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)WhereFilter).ApplySetting(settingInfo.WhereFilter);
-
-      FilterByValue = null;
-      if (settingInfo.FilterByValue != null && settingInfo.FilterByValue.Count > 0)
-      {
-        FilterByValue = new Filters.FilterList(Properties);
-        ((UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)FilterByValue).ApplySetting(settingInfo.FilterByValue);
-      }
-
-      Groups.Clear();
-      SortsOfGroups.Clear();
-      for (var i = 0; i < settingInfo.Groups.Count; i++)
-      {
-        var item = settingInfo.Groups[i];
-        var pd = Properties.Find(item.Id, true);
-        if (pd != null)
+        // 3. Add Settings for DGVList (split ApplySettings for DGVList/DGVCube); remove public sets for some DGVList properties
+        public void ResetSettings()
         {
-          Groups.Add(new ListSortDescription(pd, item.SortDirection));
-          var sortOfGroup = new List<ListSortDescription>();
-          SortsOfGroups.Add(sortOfGroup);
-          if (settingInfo.SortsOfGroup.Count > i)
-          {
-            var savedSortOfGroup = settingInfo.SortsOfGroup[i];
-            foreach (var x in savedSortOfGroup)
-            {
-              var pd1 = Properties.Find(x.Id, true);
-              if (pd1 != null)
-                sortOfGroup.Add(new ListSortDescription(pd1, x.SortDirection));
-            }
-          }
+            var whereFilter = (UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)WhereFilter;
+            whereFilter?.ApplySetting(whereFilter.GetBlankSetting());
+            var filterByValue = (UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)FilterByValue;
+            filterByValue?.ApplySetting(filterByValue.GetBlankSetting());
+            ShowTotalRow = false;
+            ExpandedGroupLevel = 0;
+            ShowGroupsOfUpperLevels = false;
+            TextFastFilter = null;
+
+            Groups?.Clear();
+            Sorts?.Clear();
+            SortsOfGroups?.Clear();
+            TotalLines = new TotalLine[0];
         }
-      }
 
-      ExpandedGroupLevel = settingInfo.ExpandedGroupLevel;
-      CurrentExpandedGroupLevel = ExpandedGroupLevel > 0 ? ExpandedGroupLevel : int.MaxValue;
-      ShowGroupsOfUpperLevels = settingInfo.ShowGroupsOfUpperLevels;
-      ShowTotalRow = settingInfo.ShowTotalRow;
+        public void SetSettings(UserSettings.DGV settingInfo)
+        {
+            TextFastFilter = settingInfo.TextFastFilter;
 
-      // Restore sorts columns
-      Sorts.Clear();
-      Sorts.AddRange(settingInfo.Sorts.Where(item => Properties.Find(item.Id, true) != null)
-        .Select(item => new ListSortDescription(Properties.Find(item.Id, true), item.SortDirection)));
+            WhereFilter.ClearFilter();
+            if (settingInfo.WhereFilter != null && settingInfo.WhereFilter.Count > 0)
+                ((UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)WhereFilter).ApplySetting(settingInfo.WhereFilter);
 
-      // Restore totals
-      Misc.TotalLine.ApplySettings(TotalLines, settingInfo.TotalLines);
-      PrepareLiveTotalLines();
+            FilterByValue = null;
+            if (settingInfo.FilterByValue != null && settingInfo.FilterByValue.Count > 0)
+            {
+                FilterByValue = new Filters.FilterList(Properties);
+                ((UserSettings.IUserSettingSupport<List<UserSettings.Filter>>)FilterByValue).ApplySetting(settingInfo.FilterByValue);
+            }
+
+            Groups.Clear();
+            SortsOfGroups.Clear();
+            for (var i = 0; i < settingInfo.Groups.Count; i++)
+            {
+                var item = settingInfo.Groups[i];
+                var pd = Properties.Find(item.Id, true);
+                if (pd != null)
+                {
+                    Groups.Add(new ListSortDescription(pd, item.SortDirection));
+                    var sortOfGroup = new List<ListSortDescription>();
+                    SortsOfGroups.Add(sortOfGroup);
+                    if (settingInfo.SortsOfGroup.Count > i)
+                    {
+                        var savedSortOfGroup = settingInfo.SortsOfGroup[i];
+                        foreach (var x in savedSortOfGroup)
+                        {
+                            var pd1 = Properties.Find(x.Id, true);
+                            if (pd1 != null)
+                                sortOfGroup.Add(new ListSortDescription(pd1, x.SortDirection));
+                        }
+                    }
+                }
+            }
+
+            ExpandedGroupLevel = settingInfo.ExpandedGroupLevel;
+            CurrentExpandedGroupLevel = ExpandedGroupLevel > 0 ? ExpandedGroupLevel : int.MaxValue;
+            ShowGroupsOfUpperLevels = settingInfo.ShowGroupsOfUpperLevels;
+            ShowTotalRow = settingInfo.ShowTotalRow;
+
+            // Restore sorts columns
+            Sorts.Clear();
+            Sorts.AddRange(settingInfo.Sorts.Where(item => Properties.Find(item.Id, true) != null)
+              .Select(item => new ListSortDescription(Properties.Find(item.Id, true), item.SortDirection)));
+
+            // Restore totals
+            Misc.TotalLine.ApplySettings(TotalLines, settingInfo.TotalLines);
+            PrepareLiveTotalLines();
+        }
+
+        public void GetSettings() { }
     }
-
-    public void GetSettings() { }
-  }
 }
--- DG.5.0/DGCore/DGVList/IDGVList.cs
+++ DG.5.0/DGCore/DGVList/IDGVList.cs
@@ -4,53 +4,53 @@ using System.ComponentModel;
 
 namespace DGCore.DGVList
 {
-  public interface IDGVList : ITypedList, IDisposable
-  {
-      Dictionary<string, string> Formats { get; }
-    PropertyDescriptorCollection Properties { get; }
-    Sql.DataSourceBase UnderlyingData { get; }
-    List<ListSortDescription> Sorts { get; }
-    List<ListSortDescription> Groups { get; }
-    List<List<ListSortDescription>> SortsOfGroups { get; }
-    Misc.TotalLine[] TotalLines { get; }
-    List<Misc.TotalLine> LiveTotalLines { get; }
-    int ExpandedGroupLevel { get; set; }
-    bool ShowGroupsOfUpperLevels { get; set; }
-    bool ShowTotalRow { get; }
-    bool IsGroupMode { get; }
-    int CurrentExpandedGroupLevel { get; }
-    int LastRefreshedTimeInMsecs { get; }
-    int FilteredRowCount { get; }
-    bool IsBindingsReseting { get; }
+    public interface IDGVList : ITypedList, IDisposable
+    {
+        Dictionary<string, string> Formats { get; }
+        PropertyDescriptorCollection Properties { get; }
+        Sql.DataSourceBase UnderlyingData { get; }
+        List<ListSortDescription> Sorts { get; }
+        List<ListSortDescription> Groups { get; }
+        List<List<ListSortDescription>> SortsOfGroups { get; }
+        Misc.TotalLine[] TotalLines { get; }
+        List<Misc.TotalLine> LiveTotalLines { get; }
+        int ExpandedGroupLevel { get; set; }
+        bool ShowGroupsOfUpperLevels { get; set; }
+        bool ShowTotalRow { get; }
+        bool IsGroupMode { get; }
+        int CurrentExpandedGroupLevel { get; }
+        int LastRefreshedTimeInMsecs { get; }
+        int FilteredRowCount { get; }
+        bool IsBindingsReseting { get; }
 
-    Filters.FilterList WhereFilter { get; }
-    Filters.FilterList FilterByValue { get; }
-    string TextFastFilter { get; }
+        Filters.FilterList WhereFilter { get; }
+        Filters.FilterList FilterByValue { get; }
+        string TextFastFilter { get; }
 
-    string[] GetSubheaders_ExcelAndPrint(string startUpParameters, string lastAppliedLayoutName);
+        string[] GetSubheaders_ExcelAndPrint(string startUpParameters, string lastAppliedLayoutName);
 
-    bool IsPropertyVisible(string propertyName);
-    bool IsGroupColumnVisible(int groupIndex);
-    // ======== Settings ============
-    void ResetSettings();
-    void SetSettings(UserSettings.DGV settingInfo);
-    // =======  Actions ===========
-    void A_SetGroupLevel(int? groupLevel, bool showUpperLevels);
-    void A_ApplySorting(string dataPropertyName, object currentDataItem, ListSortDirection sortDirection);
-    void A_RemoveSorting(string dataPropertyName, object currentDataItem);
-    void A_SetByValueFilter(string dataPropertyName, object value);
-    void A_ClearByValueFilter();
-    void A_FastFilterChanged(string newFastFilterValue);
-    // ============================
-    void RequeryData();
-    void RefreshData();
-    void ClearData();
-    void ItemExpandedChanged(int rowIndex);
+        bool IsPropertyVisible(string propertyName);
+        bool IsGroupColumnVisible(int groupIndex);
+        // ======== Settings ============
+        void ResetSettings();
+        void SetSettings(UserSettings.DGV settingInfo);
+        // =======  Actions ===========
+        void A_SetGroupLevel(int? groupLevel, bool showUpperLevels);
+        void A_ApplySorting(string dataPropertyName, object currentDataItem, ListSortDirection sortDirection);
+        void A_RemoveSorting(string dataPropertyName, object currentDataItem);
+        void A_SetByValueFilter(string dataPropertyName, object value);
+        void A_ClearByValueFilter();
+        void A_FastFilterChanged(string newFastFilterValue);
+        // ============================
+        void RequeryData();
+        void RefreshData();
+        void ClearData();
+        void ItemExpandedChanged(int rowIndex);
 
-    void ResetBindings();
-    bool NoDataFilter { get; set; }
+        void ResetBindings();
+        bool NoDataFilter { get; set; }
 
-    event Sql.DataSourceBase.dlgDataStatusChangedDelegate DataStateChanged;
-  }
+        event Sql.DataSourceBase.dlgDataStatusChangedDelegate DataStateChanged;
+    }
 
 }
