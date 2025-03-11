b75ae4a614795eb929944c1d13043661dd7e30d0
serhiy-petryk <sergei_petrik@yahoo.com>
2025-03-11 21:12:15 +0200

    reformatted some files

--- DG.5.0/DGCore/Misc/AppSettings.cs
+++ DG.5.0/DGCore/Misc/AppSettings.cs
@@ -3,33 +3,33 @@ using System.IO;
 
 namespace DGCore.Misc
 {
-  public class AppSettings
-  {
-    public const string CONFIG_FILE_NAME = @"config.json";
-    public static string settingsStorage;
-
-    static AppSettings()
+    public class AppSettings
     {
-      // Clear temp Excel files
-      // long time ClearTempFiles(Utils.ExcelApp.GetDefaultExtension());
-      ClearTempFiles("xlsx");
-      ClearTempFiles("xls");
-      ClearTempFiles("txt");
-    }
+        public const string CONFIG_FILE_NAME = @"config.json";
+        public static string settingsStorage;
 
-    static void ClearTempFiles(string fileExtension)
-    {
-      string folder = Path.GetTempPath();
-      FileInfo[] files = (new DirectoryInfo(folder)).GetFiles("DGV_*." + fileExtension, SearchOption.TopDirectoryOnly);
-      foreach (FileInfo fi in files)
-      {
-        if (fi.LastWriteTime < DateTime.Now)
+        static AppSettings()
         {
-          TimeSpan ts = DateTime.Now - fi.LastWriteTime;
-          if (ts.TotalDays > 3) fi.Delete();
+            // Clear temp Excel files
+            // long time ClearTempFiles(Utils.ExcelApp.GetDefaultExtension());
+            ClearTempFiles("xlsx");
+            ClearTempFiles("xls");
+            ClearTempFiles("txt");
         }
-      }
-    }
 
-  }
+        static void ClearTempFiles(string fileExtension)
+        {
+            string folder = Path.GetTempPath();
+            FileInfo[] files = (new DirectoryInfo(folder)).GetFiles("DGV_*." + fileExtension, SearchOption.TopDirectoryOnly);
+            foreach (FileInfo fi in files)
+            {
+                if (fi.LastWriteTime < DateTime.Now)
+                {
+                    TimeSpan ts = DateTime.Now - fi.LastWriteTime;
+                    if (ts.TotalDays > 3) fi.Delete();
+                }
+            }
+        }
+
+    }
 }
--- DG.5.0/DGCore/PD/Converters.cs
+++ DG.5.0/DGCore/PD/Converters.cs
@@ -3,101 +3,102 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Text;
 
-namespace DGCore.PD {
-  public class ByteArrayToHexStringConverter : TypeConverter
-  {
-    public static void Test()
+namespace DGCore.PD
+{
+    public class ByteArrayToHexStringConverter : TypeConverter
     {
-      var c = new ByteArrayToHexStringConverter();
-      var bb = new byte[] { 0, 1, 2, 255 };
-      var s = c.ConvertFrom(bb);
-      var z = c.ConvertTo("0x55", typeof(byte[]));
+        public static void Test()
+        {
+            var c = new ByteArrayToHexStringConverter();
+            var bb = new byte[] { 0, 1, 2, 255 };
+            var s = c.ConvertFrom(bb);
+            var z = c.ConvertTo("0x55", typeof(byte[]));
+        }
+        //-----------------------
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) => sourceType == typeof(byte[]);
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) => destinationType == typeof(byte[]);
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value == null || value == DBNull.Value)
+                return null;
+
+            if (!(value is byte[])) return base.ConvertFrom(context, culture, value);
+
+            var bb = (byte[])value;
+            var hex = new StringBuilder("0x", bb.Length * 2 + 2);
+            foreach (var b in bb)
+                hex.AppendFormat("{0:X2}", b);
+            return hex.ToString();
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (value == null || value == DBNull.Value)
+                return (byte[])null;
+
+            if (destinationType != typeof(byte[]) || !(value is string))
+                return base.ConvertTo(context, culture, value, destinationType);
+
+            var hex = (string)value;
+            if (!hex.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+                throw new Exception("String have to start with '0x' prefix");
+            if ((hex.Length % 2) != 0)
+                throw new Exception("Number of characters in string must be even");
+
+            var cnt = 1;
+            foreach (var c in hex.Substring(2))
+            {
+                if (!IsHexSymbol(c))
+                    throw new Exception($"{cnt}-th character of hex string ('{c}') is not hexadecimal symbol");
+                cnt++;
+            }
+
+            var numberChars = hex.Length - 2;
+            var bytes = new byte[numberChars / 2];
+            for (var i = 0; i < numberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i + 2, 2), 16);
+
+            return bytes;
+        }
+
+        private static bool IsHexSymbol(char c) => ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'));
     }
-    //-----------------------
-    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) => sourceType == typeof(byte[]);
-    public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) => destinationType == typeof(byte[]);
 
-    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    //==============  ByteArrayToGuidStringConverter  ================
+    public class ByteArrayToGuidStringConverter : TypeConverter
     {
-      if (value == null || value == DBNull.Value)
-        return null;
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) => sourceType == typeof(byte[]);
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) => destinationType == typeof(byte[]);
 
-      if (!(value is byte[])) return base.ConvertFrom(context, culture, value);
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value == null || value == DBNull.Value)
+                return null;
 
-      var bb = (byte[])value;
-      var hex = new StringBuilder("0x", bb.Length * 2 + 2);
-      foreach (var b in bb)
-        hex.AppendFormat("{0:X2}", b);
-      return hex.ToString();
+            if (!(value is byte[])) return base.ConvertFrom(context, culture, value);
+
+            return (new Guid((byte[])value)).ToString();
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (value == null || value == DBNull.Value)
+                return (byte[])null;
+
+            if (destinationType != typeof(byte[]) || !(value is Guid))
+                return base.ConvertTo(context, culture, value, destinationType);
+
+            return Guid.Parse(value.ToString()).ToByteArray();
+        }
     }
 
-    public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-    {
-      if (value == null || value == DBNull.Value)
-        return (byte[])null;
 
-      if (destinationType != typeof(byte[]) || !(value is string))
-        return base.ConvertTo(context, culture, value, destinationType);
-
-      var hex = (string)value;
-      if (!hex.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
-        throw new Exception("String have to start with '0x' prefix");
-      if ((hex.Length % 2) != 0)
-        throw new Exception("Number of characters in string must be even");
-
-      var cnt = 1;
-      foreach (var c in hex.Substring(2))
-      {
-        if (!IsHexSymbol(c))
-          throw new Exception($"{cnt}-th character of hex string ('{c}') is not hexadecimal symbol");
-        cnt++;
+    /*//======================  DummyEditor   ========================
+    public class DummyEditor : UITypeEditor {
+      public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) {
+        return UITypeEditorEditStyle.None;
       }
-
-      var numberChars = hex.Length - 2;
-      var bytes = new byte[numberChars / 2];
-      for (var i = 0; i < numberChars; i += 2)
-        bytes[i / 2] = Convert.ToByte(hex.Substring(i + 2, 2), 16);
-
-      return bytes;
-    }
-
-    private static bool IsHexSymbol(char c) => ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'));
-  }
-
-  //==============  ByteArrayToGuidStringConverter  ================
-  public class ByteArrayToGuidStringConverter : TypeConverter
-  {
-    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) => sourceType == typeof(byte[]);
-    public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) => destinationType == typeof(byte[]);
-
-    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-    {
-      if (value == null || value == DBNull.Value)
-        return null;
-
-      if (!(value is byte[])) return base.ConvertFrom(context, culture, value);
-
-      return (new Guid((byte[])value)).ToString();
-    }
-
-    public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-    {
-      if (value == null || value == DBNull.Value)
-        return (byte[])null;
-
-      if (destinationType != typeof(byte[]) || !(value is Guid))
-        return base.ConvertTo(context, culture, value, destinationType);
-
-      return Guid.Parse(value.ToString()).ToByteArray();
-    }
-  }
-
-
-  /*//======================  DummyEditor   ========================
-  public class DummyEditor : UITypeEditor {
-    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) {
-      return UITypeEditorEditStyle.None;
-    }
-  }*/
+    }*/
 
 }
