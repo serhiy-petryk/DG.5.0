03a3fa1ccfb02d79b69710407440c1a509bdb864
serhiy-petryk <sergei_petrik@yahoo.com>
2025-03-11 21:09:39 +0200

    reformatted some files

--- DG.5.0/DGCore/Menu/SqlObject.cs
+++ DG.5.0/DGCore/Menu/SqlObject.cs
@@ -5,57 +5,57 @@ using System.Runtime.Versioning;
 
 namespace DGCore.Menu
 {
-  [SupportedOSPlatform("windows")]
-  public class SqlObject
-  {
-    public string CS { get; set; }
-    public RootMenu.DbConnection oCS { get; set; }
-    public string Sql { get; set; }
-
-    private Dictionary<string, DbParameter> _parameters = new Dictionary<string, DbParameter>(StringComparer.OrdinalIgnoreCase);
-    public Dictionary<string, DbParameter> Parameters
+    [SupportedOSPlatform("windows")]
+    public class SqlObject
     {
-      get => _parameters;
-      set => _parameters = GetDictionaryWithOrdinalIgnoreCaseComparer(value);
+        public string CS { get; set; }
+        public RootMenu.DbConnection oCS { get; set; }
+        public string Sql { get; set; }
+
+        private Dictionary<string, DbParameter> _parameters = new Dictionary<string, DbParameter>(StringComparer.OrdinalIgnoreCase);
+        public Dictionary<string, DbParameter> Parameters
+        {
+            get => _parameters;
+            set => _parameters = GetDictionaryWithOrdinalIgnoreCaseComparer(value);
+        }
+
+        private Dictionary<string, RootMenu.Column> _columns = new Dictionary<string, RootMenu.Column>(StringComparer.OrdinalIgnoreCase);
+        public Dictionary<string, RootMenu.Column> Columns
+        {
+            get => _columns;
+            set => _columns = GetDictionaryWithOrdinalIgnoreCaseComparer(value);
+        }
+
+        private static Dictionary<string, TValue> GetDictionaryWithOrdinalIgnoreCaseComparer<TValue>(Dictionary<string, TValue> data)
+        {
+            if (data.Comparer == StringComparer.OrdinalIgnoreCase) return data;
+            return data.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public string SqlForColumnAttributes { get; set; }
+        public string ItemType { get; set; }
+        public Type oItemType { get; set; }
+
+        public void SetConnectionObject(Dictionary<string, RootMenu.DbConnection> dbConnections)
+        {
+            if (oCS == null && !string.IsNullOrEmpty(CS))
+                oCS = dbConnections[CS.Trim()];
+        }
+
+        public void Normalize(RootMenu.MainObject mo)
+        {
+            if (!string.IsNullOrEmpty(ItemType))
+            {
+                oItemType = Utils.Types.TryGetType(ItemType);
+                if (oItemType == null)
+                    throw new Exception($"Can not find item type: {ItemType}");
+            }
+
+            foreach (var kvp in Parameters)
+                kvp.Value.Normalize(kvp.Key, mo);
+
+            foreach (var kvp in Columns)
+                kvp.Value.Normalize(kvp.Key, mo);
+        }
     }
-
-    private Dictionary<string, RootMenu.Column> _columns = new Dictionary<string, RootMenu.Column>(StringComparer.OrdinalIgnoreCase);
-    public Dictionary<string, RootMenu.Column> Columns
-    {
-      get => _columns;
-      set => _columns = GetDictionaryWithOrdinalIgnoreCaseComparer(value);
-    }
-
-    private static Dictionary<string, TValue> GetDictionaryWithOrdinalIgnoreCaseComparer<TValue>(Dictionary<string, TValue> data)
-    {
-      if (data.Comparer == StringComparer.OrdinalIgnoreCase) return data;
-      return data.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
-    }
-
-    public string SqlForColumnAttributes { get; set; }
-    public string ItemType { get; set; }
-    public Type oItemType { get; set; }
-
-    public void SetConnectionObject(Dictionary<string, RootMenu.DbConnection> dbConnections)
-    {
-      if (oCS == null && !string.IsNullOrEmpty(CS))
-        oCS = dbConnections[CS.Trim()];
-    }
-
-    public void Normalize(RootMenu.MainObject mo)
-    {
-      if (!string.IsNullOrEmpty(ItemType))
-      {
-        oItemType = Utils.Types.TryGetType(ItemType);
-        if (oItemType == null)
-          throw new Exception($"Can not find item type: {ItemType}");
-      }
-
-      foreach (var kvp in Parameters)
-        kvp.Value.Normalize(kvp.Key, mo);
-
-      foreach (var kvp in Columns)
-        kvp.Value.Normalize(kvp.Key, mo);
-    }
-  }
 }
